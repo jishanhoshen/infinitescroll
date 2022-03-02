b39b27666cfa6d83590367e79a0cbf5f147b7371
jishanhoshen <70538593+jishanhoshen@users.noreply.github.com>
2022-03-02 18:38:43 +0600

    v0.2

--- build.php
+++ build.php
@@ -1,7 +1,7 @@
 <?php
 include "config.php";
 
-$build_item = 100;
+$build_item = 99;
 
 $img_url = "https://picsum.photos/v2/list?page=2&limit=".$build_item;
 $unsplash = json_decode(file_get_contents($img_url),true);
@@ -10,9 +10,13 @@ for ($i = 0; $i < $build_item; $i++) {
     $get_title = file_get_contents("http://loripsum.net/api/1/short/plaintext", true);
     $title = substr($get_title, 0, 60);
     $content = file_get_contents("http://loripsum.net/api/" . rand(0, 9) . "/short/plaintext", true);
-    $time = time();
     $image = $unsplash[$i]['download_url'];
-    $query = 'INSERT INTO `blog_post` (title,content,post_date,img) VALUES ("'.$title.'","'.$content.'","'.$time.'","'.$image.'")';
-    $conn->query($query);
+    $query = 'INSERT INTO `blog_post` (title,content,img) VALUES ("'.$title.'","'.$content.'","'.$image.'")';
+    // $conn->query($query);
     // echo $query;
+    if(!$conn->query($query)){
+        echo 'something went wrong';
+    }else{
+        header('location:index.html');
+    }
 }
--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "blog";
+$servername = "remotemysql.com";
+$username = "uL1ayWPKOh";
+$password = "TurxFpmYcr";
+$database = "uL1ayWPKOh";
 $conn = new mysqli($servername,$username,$password,$database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
